a820e374d767b4abb08002bef01bdf448dec713a
Pynshai <62445242+pynshaixcx@users.noreply.github.com>
2023-07-19 10:31:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+from RPS_game import play, pynshai, mei2, quincy, dari, human, random_player
+from RPS import player
+from unittest import main
 
+#play(player, quincy, 1000)
+#play(player, mei2, 1000)
+#play(player, dari, 1000)
+#play(player, pynshai, 1000)
+
+
+
+
+# Uncomment line below to play interactively against a bot:
+#play(human, mei2, 20, verbose=True)
+
+# Uncomment line below to play against a bot that plays randomly:
+# play(human, random_player, 1000)
+
+
+
+# Uncomment line below to run unit tests automatically
+main(module='test_module', exit=False)
